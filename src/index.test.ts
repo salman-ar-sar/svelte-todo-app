1845607f1d12ba2822094cecd8e6e7383e13c7a9
Salman A <salmana.ar@gmail.com>
2023-08-13 18:25:44 +0530

    feat: Configure testing

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,11 @@
 import { describe, it, expect } from 'vitest';
+import { render } from '@testing-library/svelte';
+import Header from './routes/Header.svelte';
 
-describe('sum test', () => {
-	it('adds 1 + 2 to equal 3', () => {
-		expect(1 + 2).toBe(3);
+describe('Tests Header', () => {
+	it('mounts', () => {
+		const { container } = render(Header);
+		expect(container).toBeTruthy();
+		expect(container.innerHTML).toMatchSnapshot();
 	});
 });
